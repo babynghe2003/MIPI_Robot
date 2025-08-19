16bde06fabe26d0c5e0c4f1a0346ee51c44fbcb0
minhphi <himinhpho@gmail.com>
2025-08-20 00:08:51 +0700

    ok esp_simplefoc

--- main/include/app_config.h
+++ main/include/app_config.h
@@ -14,7 +14,7 @@
 #define I2C_MASTER_SCL_IO           GPIO_NUM_9     // I2C master clock (ESP32-S3)
 #define I2C_MASTER_SDA_IO           GPIO_NUM_8     // I2C master data (ESP32-S3)
 #define I2C_MASTER_NUM              I2C_NUM_0     // I2C master i2c port number
-#define I2C_MASTER_FREQ_HZ          100000  // I2C master clock frequency
+#define I2C_MASTER_FREQ_HZ          400000  // I2C master clock frequency
 #define I2C_MASTER_TX_BUF_DISABLE   0     // I2C master doesn't need buffer
 #define I2C_MASTER_RX_BUF_DISABLE   0     // I2C master doesn't need buffer
 #define I2C_MASTER_TIMEOUT_MS       1000
--- main/modules/sensors/robot_task.cpp
+++ main/modules/sensors/robot_task.cpp
@@ -10,6 +10,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
+#include "esp_simplefoc.h"
 
 // Custom sensor includes
 extern "C" {
